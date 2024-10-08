0758a1fef7242477283c00250e03cb29fb8c7f7a
Denis Kovalyonok <deniskovalyonok@gmail.com>
2024-10-08 22:00:48 +0200

    PolicySearchService API added.

---
+++ DK.PolicySearchService.API/Queries/FindPolicyQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace DK.PolicySearchService.API.Queries;
+
+public class FindPolicyQuery : IRequest<FindPolicyResult>
+{
+    public string QueryText { get; set; }
+}
---
+++ DK.PolicySearchService.API/Queries/FindPolicyResult.cs
@@ -0,0 +1,8 @@
+﻿using DK.PolicySearchService.API.Queries.Dtos;
+
+namespace DK.PolicySearchService.API.Queries;
+
+public class FindPolicyResult
+{
+    public List<PolicyDto> Policies { get; set; }
+}
